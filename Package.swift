d2b9460117b501ab497f82cded3dd6958a876231
urlocaluwu <48158580+urlocaluwu@users.noreply.github.com>
2024-10-08 11:52:35 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -15,7 +15,6 @@ let package = Package(
         .library(name: "DiscordKitBot", targets: ["DiscordKitBot"]) // Bot-oriented module, for use in bots
 	],
 	dependencies: [
-        .package(url: "https://github.com/ashleymills/Reachability.swift", from: "5.1.0"),
         .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0"),
         .package(url: "https://github.com/apple/swift-protobuf.git", from: "1.6.0"),
         .package(url: "https://github.com/apple/swift-log.git", from: "1.0.0"),
